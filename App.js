2ad02e87896fc434b828c8edf98bfa22ddf82b29
Islam Dudaev <islamwolf23@gmail.com>
2018-06-28 17:29:21 +0100

    create state, add handleAddItem function

--- App.js
+++ App.js
@@ -7,13 +7,40 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      items: []
+    };
+    this.handleAddItem = this.handleAddItem.bind(this);
+  }
+
+  handleAddItem() {
+    if (!this.state.value) return;
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ];
+    this.setState({
+      items: newItems,
+      value: ""
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-        
-        </View>
+        <Header
+          value={this.state.value}
+          onAddItem={this.handleAddItem}
+          onChange={value => this.setState({ value })}
+        />
+        <View style={styles.content} />
         <Footer />
       </View>
     );
